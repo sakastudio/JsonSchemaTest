7fbdc9a3e6f98c698e7d487ddf97243bbd90db08
sakastudio <katsumi.sato@aktsk.jp>
2024-06-13 13:16:17 +0900

    とりあえずロードできた

--- JsonSchemaTest/Program.cs
+++ JsonSchemaTest/Program.cs
@@ -1,3 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using System.Text.Json;
+using Json.Schema;
+
+
+var schema = JsonSchema.FromFile("../../../testschema.json");
+
+Console.WriteLine(schema != null ? "Schema loaded successfully" : "Failed to load schema");
