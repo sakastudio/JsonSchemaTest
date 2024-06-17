7c4bcfef7fb99923cf3b6cd3a02196c2fa9b78d5
sakastudio <katsumi.sato@aktsk.jp>
2024-06-17 22:18:16 +0900

    キーワード取得してるけどうまくいかな

--- JsonSchemaTest/Program.cs
+++ JsonSchemaTest/Program.cs
@@ -6,4 +6,28 @@ using Json.Schema;
 
 var schema = JsonSchema.FromFile("../../../testschema.json");
 
-Console.WriteLine(schema != null ? "Schema loaded successfully" : "Failed to load schema");
+CheckProperty(schema);
+
+void CheckProperty(JsonSchema currentSchema)
+{
+    var keywords = currentSchema.Keywords;
+    if (keywords == null)
+    {
+        return;
+    }
+
+    foreach (var keyword in keywords)
+    {
+        if (keyword.Keyword() != PropertiesKeyword.Name) continue;
+
+        var propertiesKeyword = (PropertiesKeyword)keyword;
+        foreach (var property in propertiesKeyword.Properties)
+        {
+            var key = property.Key;
+            var value = property.Value;
+
+            Console.WriteLine($"Key: {key}");
+            CheckProperty(value);
+        }
+    }
+}
